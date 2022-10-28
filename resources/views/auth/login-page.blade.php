bcccffb1e3d3649b704507718fbed3d5dc385b78
IuriOliveira7 <iurioliveira7@hotmail.com>
2022-10-28 20:37:55 -0300

    Projeto SQL

--- resources/views/auth/login-page.blade.php
+++ resources/views/auth/login-page.blade.php
@@ -29,7 +29,8 @@
                 <input class="input100" type="text" name="email" placeholder="Email">
                 <span class="focus-input100"></span>
                 <span class="symbol-input100">
-                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                     <i class="fa fa-envelope" aria-hidden="true"></i>
+                     
                 </span>
             </div>
 
